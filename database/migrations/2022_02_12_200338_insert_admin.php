6858e4bca87582482c4845c2f0e63f1b556b265c
Luiz Antonio <luiz8907@hotmail.com>
2022-02-12 17:08:45 -0300

    Initial commit

---
+++ database/migrations/2022_02_12_200338_insert_admin.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       DB::table('users')->insert([
+           'name' => 'Administrador',
+           'email' => '',
+           'password' => ''
+       ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
